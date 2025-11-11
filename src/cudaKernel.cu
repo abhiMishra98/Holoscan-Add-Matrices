44650977c9a14d43dfc5e21df66e5687df0df3a8
abhiMishra98 <abhimishra.cetb@gmail.com>
2025-11-11 17:21:05 +0100

    Move device mem alloc to opOut, add start() in opInit,add stop() to free raw ptrs

--- src/cudaKernel.cu
+++ src/cudaKernel.cu
@@ -2,17 +2,11 @@
 #include <cuda_runtime.h>
 #include "cudaKernel.h"
 
-
 __global__ void add( int N, int *a, int *b){
     int index = blockIdx.x*blockDim.x + threadIdx.x;
     int stride = blockDim.x*gridDim.x;
-
-    for(int i = index; i < N; i+=stride){
-      int temp = a[i] + b[i];
-        for (int j = 0; j < 1000; ++j) {
-            temp += j % 5;
-        }
-        a[i] = temp;
+    for (int i = index; i < N; i += stride) {
+        a[i] += b[i];
     }
 }
 
@@ -27,15 +21,16 @@ void addMatrix(int *a, int *b, int N){
 
     int blockSize=256;
     int numBlocks = (N+blockSize-1)/blockSize;
-    add<<<numBlocks,blockSize>>>(N,a,b);
-    //add<<<1,1>>>(N,a,b);
+   
 
     cudaMemPrefetchAsync(a, N * sizeof(int),0,0);
     cudaMemPrefetchAsync(b, N * sizeof(int),0,0);
 
+    add<<<numBlocks,blockSize>>>(N,a,b);
+    //add<<<1,1>>>(N,a,b);
+
     cudaDeviceSynchronize();
 
-    cudaFree(a);
-    cudaFree(b);
+
     
 }
--- src/cudaTest.cpp
+++ src/cudaTest.cpp
@@ -2,18 +2,36 @@
 #include "cudaKernel.h"
 #include <cuda_runtime.h>
 #include <iostream>
+#include <memory>
+
+#define CUDA_CHECK(call) \
+    do { cudaError_t err = call; \
+         if(err != cudaSuccess){ \
+             std::cerr << "CUDA Error: " << cudaGetErrorString(err) \
+                       << " at " << __FILE__ << ":" << __LINE__ << std::endl; \
+             exit(EXIT_FAILURE); \
+         } \
+    } while(0)
+
 
 namespace holoscan::ops{
     class opInit: public Operator{
         private:
-            int *a,*b;
+            int *a = nullptr;
+            int *b = nullptr;
             int N;
+            std::shared_ptr<int> num_ptr;
+            
         public:
             HOLOSCAN_OPERATOR_FORWARD_ARGS(opInit);
             // To check if this way of declaring a constructor is fine or not
-            opInit(){
-                N= 1<<20;
+            void start() override {
+                
+                    N= 1<<20;
+                // num_ptr = std::make_shared<int>(N);
+
             }
+
 
             void setup(OperatorSpec &spec) override{
                 spec.output<int *>("outFirst");
@@ -23,17 +41,35 @@ namespace holoscan::ops{
 
             void compute(InputContext &in, OutputContext &op, ExecutionContext &ex) override{
                 //To check if CUDA APIs can be called or not and also if initMatrix can be called successfully
-                cudaMallocManaged(&a,sizeof(int)*N);
-                cudaMallocManaged(&b,sizeof(int)*N);
+                cudaSetDevice(0);
+                a = new int[N];
+                b = new int[N];
                 initMatrix(a,b,N);
 
+               // cudaMallocManaged(&a,sizeof(int)*N);
+               // cudaMallocManaged(&b,sizeof(int)*N);
+                
                 op.emit(a,"outFirst");
                 op.emit(b,"outSecond");
-                op.emit(N,"outNumbers");
+                //op.emit(num_ptr,"outNumbers");
+                std::cout<<"N is "<<N<<std::endl;
+                op.emit(N, "outNumbers");
+            }
+
+            void stop() override {
+                if(a){
+                    delete[] a;
+                }
+                if(b){
+                    delete[] b;
+                }
+                
             }
     };
 
     class opOut: public Operator{
+        private:
+            int *d_a,*d_b;
         public:
             HOLOSCAN_OPERATOR_FORWARD_ARGS(opOut);
             opOut() = default;
@@ -49,10 +85,20 @@ namespace holoscan::ops{
                 auto ipSecond = ip.receive<int *>("inSecond");
                 auto inNumbers = ip.receive<int>("inNumbers");
 
+               int No = (inNumbers.value());
+                std::cout<<"N is "<<No<<std::endl;
+                CUDA_CHECK(cudaMalloc(&d_a, No * sizeof(int)));
+                CUDA_CHECK(cudaMalloc(&d_b, No * sizeof(int)));
                 int *a = ipFirst.value();
                 int *b = ipSecond.value();
-                int N = inNumbers.value();
-                addMatrix(a,b,N);
+                CUDA_CHECK(cudaMemcpy(d_a,a,No*sizeof(int),cudaMemcpyHostToDevice));
+                CUDA_CHECK(cudaMemcpy(d_b,b,No*sizeof(int),cudaMemcpyHostToDevice));
+
+                addMatrix(d_a,d_b,No);
+
+                cudaDeviceSynchronize();
+                cudaFree(d_a);
+                cudaFree(d_b);
             }
             
     };
@@ -79,5 +125,6 @@ class perfCal: public holoscan::Application{
 int main() {
     auto myApp = holoscan::make_application<perfCal>();
     myApp->run();
+
     return 0;
 }
